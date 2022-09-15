7af5b710dc70da18a4640a45f76df3d82a462413
Lenin Capa <leviskp87@gmail.com>
2022-09-15 16:49:40 -0500

    add stellar map page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StellarMapPageComponent } from './pages/stellar-map-page/stellar-map-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'stellar-editor', component: StellarMapPageComponent, pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
