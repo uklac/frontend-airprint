583842402349404c7910bdc0f89d73c1ca1f949b
ricardokpa <64665611+ricardokpa@users.noreply.github.com>
2022-09-16 17:04:29 -0500

    add page-404-error (#7)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StellarMapPageComponent } from './pages/stellar-map-page/stellar-map-page.component';
+import { ErrorPageComponent  } from './pages/error-page/error-page.component';
 
 const routes: Routes = [
   { path: 'stellar-editor', component: StellarMapPageComponent, pathMatch: 'full'},
+  { path: '404', component: ErrorPageComponent, pathMatch: 'full'},
 ];
 
 @NgModule({
