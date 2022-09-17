8d28129b21bce8ef8b0e4e6fa5f2764c10c6bccd
Lenin Capa <leviskp87@gmail.com>
2022-09-17 11:02:24 -0500

    create select theme component

---
+++ src/app/components/select-theme/select-theme.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SelectThemeComponent } from './select-theme.component';
+
+describe('SelectThemeComponent', () => {
+  let component: SelectThemeComponent;
+  let fixture: ComponentFixture<SelectThemeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SelectThemeComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SelectThemeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
