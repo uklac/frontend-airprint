01648b992a062dbe99891e085f57ec7ef43510ad
Lenin Capa <leviskp87@gmail.com>
2022-09-24 17:35:15 -0500

    adding categories

--- src/app/pages/geometric-posters-pages/geometric-posters-pages.component.ts
+++ src/app/pages/geometric-posters-pages/geometric-posters-pages.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Category {
+	key: string;
+	name: string;
+}
+
 @Component({
   selector: 'app-geometric-posters-pages',
   templateUrl: './geometric-posters-pages.component.html',
@@ -9,7 +14,33 @@ export class GeometricPostersPagesComponent implements OnInit {
 
   constructor() { }
 
+	categorySelected = '';
+
+	defaultProps = {
+		headline: 'MARCEL BREURER',
+    phrase: 'Designer from 1995',
+    style: {
+      background: '#EFEFEF',
+      text: '#182731'
+    }
+	}
+
+	categories: Array<Category> = [
+		{
+			key: 'bauhaus',
+			name: 'Bauhaus'
+		},
+		{
+			key: 'pattern',
+			name: 'Patterns'
+		}
+	]
+
   ngOnInit(): void {
   }
 
+	onSelectCategory(category: Category) {
+		debugger
+		this.categorySelected = category.key
+	}
 }
