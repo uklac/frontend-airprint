850b7c2a1a72de5421bd64aec2cc7d77f41a313f
Lenin Capa <leviskp87@gmail.com>
2022-10-07 21:02:56 -0500

    add new components

---
+++ src/app/components/frame-container/frame-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-frame-container',
+  templateUrl: './frame-container.component.html',
+  styleUrls: ['./frame-container.component.scss']
+})
+export class FrameContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/signature-poster-preview/signature-poster-preview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signature-poster-preview',
+  templateUrl: './signature-poster-preview.component.html',
+  styleUrls: ['./signature-poster-preview.component.scss']
+})
+export class SignaturePosterPreviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
