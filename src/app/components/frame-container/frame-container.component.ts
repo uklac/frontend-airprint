59f9a6c877b8aa8f6c8dfc1bc58b7d3a6cfcceb9
Lenin Capa <leviskp87@gmail.com>
2022-10-07 21:16:23 -0500

    add params

--- src/app/components/frame-container/frame-container.component.ts
+++ src/app/components/frame-container/frame-container.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+export interface Frame {
+  url: string;
+}
 
 @Component({
   selector: 'app-frame-container',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./frame-container.component.scss']
 })
 export class FrameContainerComponent implements OnInit {
+	@Input() params: Frame | undefined;
+	styleFrame = {};
 
   constructor() { }
 
   ngOnInit(): void {
+		if (this.params) {
+			const { url } = this.params;
+			this.styleFrame = {
+				'background': `url(http://tumodaurbana.com/wp-content/uploads/2013/05/tumodaurbana-expo-dali.jpg)`,
+				'border-image': `url(${url}) 200 stretch`
+			}
+		}
   }
+
+	ngOnChanges(changes: SimpleChanges): void {
+		if(changes['colorFrame'] && !changes['colorFrame'].firstChange){
+      const {colorFrame: { currentValue } } = changes;
+			this.styleFrame = {
+				'background': `url(http://tumodaurbana.com/wp-content/uploads/2013/05/tumodaurbana-expo-dali.jpg)`,
+				'border-image': `url(${currentValue.url}) 200 stretch`
+			}
+    }
+	}
 
 }
